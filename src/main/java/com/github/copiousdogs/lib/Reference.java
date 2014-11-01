1e591f7072bdd7b790a2e5ae77a2f2561347caaa
Dramentiaras <martin.jirlow@gmail.com>
2014-11-01 15:39:57 +0100

    Fixed versioning

--- src/main/java/com/github/copiousdogs/lib/Reference.java
+++ src/main/java/com/github/copiousdogs/lib/Reference.java
@@ -6,7 +6,7 @@ public class Reference
 {
 	public static final String MOD_NAME = "Copious Dogs";
 	public static final String MOD_ID = "CopiousDogs";
-	public static final String VERSION = "1.7.10-3.0";
+	public static final String VERSION = "${mcversion}-${version}";
 	public static final String CHANNEL_NAME = "CopiousDogs#1";
 	
 	public static final String CLIENT_PROXY_CLASS = "com.github.copiousdogs.client.ClientProxy";
